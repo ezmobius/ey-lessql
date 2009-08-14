929efc07517a1528191b3294ef1d8ed6aa9ede0d
Lyric Hartley <lyric@lyric-hartleys-macbook-pro-2.local>
2009-08-15 05:47:52 +0800

    starting the cassandra scripts
    Signed-off-by: ezmobius <ez@engineyard.com>

--- cookbooks/cassandra/recipes/default.rb
+++ cookbooks/cassandra/recipes/default.rb
@@ -2,3 +2,30 @@
 # Cookbook Name:: cassandra
 # Recipe:: default
 #
+
+package "sun-java6-jre" do
+  action :install
+end
+
+directory "/mnt/cassandra" do
+  owner node[:owner_name]
+  group node[:owner_name]
+  mode 0755
+  recursive true
+end
+
+remote_file "/tmp/cassandra.tgz" do
+  source "http://apache.mirror.facebook.net/incubator/cassandra/0.3.0/apache-cassandra-incubating-0.3.0-bin.tar.gz"
+  mode "0644"
+  checksum "bfbb9cd29866ac31217f72c57e277a67" # A SHA256 (or portion thereof) of the file.
+end
+
+template "/etc/cassandra/storage-conf.xml" do
+  owner 'root'
+  group 'root'
+  mode 0644
+  source "storage-conf.xml.erb"
+  variables({
+    :basedir => '/mnt/cassandra',
+  })
+end
