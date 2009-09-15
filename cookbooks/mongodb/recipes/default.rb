a3bed5209e82774c1f0ea1d21170250790bc2373
Ezra Zygmuntowicz <ez@engineyard.com>
2009-09-15 12:17:12 -0700

    updating mongodb recipe from hornbeck

--- cookbooks/mongodb/recipes/default.rb
+++ cookbooks/mongodb/recipes/default.rb
@@ -1,29 +1,65 @@
 #
 # Cookbook Name:: mongodb
 # Recipe:: default
-#
 
-mongoversion = '0.9.5'
 
-directory "/db/mongodb" do
-  owner "root"
-  group "root"
+directory "/data/master" do
+  owner node[:owner_name]
+  group node[:owner_name]
   mode 0755
   recursive true
 end
 
-execute "download-and-install-mongodb" do
-  command %Q{
-    curl -O http://downloads.mongodb.org/linux/mongodb-linux-i686-#{mongoversion}.tgz &&
-    tar xzf mongodb-linux-i686-#{mongoversion}.tgz &&
-    cd mongodb-linux-i686-#{mongoversion} &&
-    mkdir -p /usr/local/include &&
-    mkdir -p /usr/local/lib &&
-    mkdir -p /usr/local/bin &&
-    cp -R bin/* /usr/local/bin/ &&
-    cp -R include/* /usr/local/include/ &&
-    cp -R lib/* /usr/local/lib/
-  }
-  not_if { File.exist?("/usr/local/bin/mongod")}
+directory "/data/slave" do
+  owner node[:owner_name]
+  group node[:owner_name]
+  mode 0755
+  recursive true
 end
+  
+execute "install-mongodb" do
+  command %Q{
+    curl -O http://downloads.mongodb.org/linux/mongodb-linux-x86_64-1.0.0.tgz &&
+    tar zxvf mongodb-linux-x86_64-1.0.0.tgz &&
+    mv mongodb-linux-x86_64-1.0.0 /usr/local/mongodb &&
+    rm mongodb-linux-x86_64-1.0.0.tgz
+  }
+  not_if { File.directory?('/usr/local/mongodb') }
+end
+  
+execute "add-to-path" do
+  command %Q{
+    echo 'export PATH=$PATH:/usr/local/mongodb/bin' >> /etc/profile
+  }
+  not_if "grep 'export PATH=$PATH:/usr/local/mongodb/bin' /etc/profile"
+end
+  
+execute "install-mongomapper" do
+  command %Q{
+    gem install jnunemaker-mongomapper --source http://gems.github.com
+  }
+end
+
+remote_file "/etc/init.d/mongodb" do
+  source "mongodb"
+  owner "root"
+  group "root"
+  mode 0755
+end
+
+execute "add-mongodb-to-default-run-level" do
+  command %Q{
+    rc-update add mongodb default
+  }
+  not_if "rc-status | grep mongodb"
+end
+
+execute "ensure-mongodb-is-running" do
+  command %Q{
+    /etc/init.d/mongodb start
+  }
+  not_if "pgrep mongod"
+end
+
+
 
